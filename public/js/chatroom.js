375633b4de466623f128a21b903d7a7338d086d4
George Horrell <ghorrell@stanford.edu>
2016-07-11 14:41:21 -0700

    moved js away from view

---
+++ public/js/chatroom.js
@@ -0,0 +1,18 @@
+function updateScroll() {
+	var element = document.getElementById("messages");
+	element.scrollTop = element.scrollHeight + 20;
+}
+
+var socket = io();
+$('form').submit(function() {
+	socket.emit('chat message', $('#send').val());
+	$('#send').val('');
+	return false;
+});
+socket.on('new sender', function(sender) {
+	$('#messages').append($('<p>').text(sender));
+});
+socket.on('chat message', function(msg) {
+	$('#messages').append($('<li>').text(msg));
+	updateScroll();	
+});
