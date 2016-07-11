1a65874d9d19e496c2ac3f9565b700d191cadce3
George Horrell <ghorrell@stanford.edu>
2016-07-10 19:37:06 -0700

    changed port

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ io.on('connection', function(socket) {
 	
 });
 
-http.listen(3000, function() {
-	console.log('listening on *:3000');
+http.listen(8081, function() {
+	console.log('listening on *:8081');
 });
